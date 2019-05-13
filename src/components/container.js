6b7de065b1529651d147b76c7dc08de71bacc5c6
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2019-05-13 21:47:57 +0800

    Hooks Conversion

--- src/components/container.js
+++ src/components/container.js
@@ -1,59 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { ThemeContext, themes } from './theme-context';
 import View from './view';
 import '../style/index.scss';
 
-class Container extends React.Component {
+const Container = () => {
 
-	state = {
-		text: "",
-		items: [],
-		count: 0,
-		theme: themes.light,
-	}
+	const [theme, setTheme] = useState(themes.light);
 
-	addItem = e => {
-		e.preventDefault();
-		this.setState({
-			items: [...this.state.items, this.state.text],
-			count: this.state.count + 1, 
-			text: "",
-		});
-	}
-
-	updateText = e => {
-		this.setState({text: e.target.value});
-	}
-
-	changeTheme = () =>{
-		if(this.state.theme === themes.light){
-			this.setState({theme: themes.dark});
+	function toggleTheme(){
+		if(theme === themes.light){
+			setTheme(themes.dark);
 		}
 		else {
-			this.setState({theme: themes.light});
+			setTheme(themes.light);
 		}
 	}
 
-	componentDidMount(){
-		document.title = `You have ${this.state.count} tasks!`;
-	}
-
-	componentDidUpdate(prevProps, prevState){
-		document.title = `You have ${this.state.count} tasks!`;
-	} 
-
-    render(){
-    	return (
-	    	<ThemeContext.Provider value={this.state.theme}>
-	    		<View
-	    			{...this.state}
-	    			addItem={this.addItem}
-	    			changeTheme={this.changeTheme}
-	    			updateText={this.updateText}
-	    		/>
-	    	</ThemeContext.Provider>
-        );
-    }
+	return (
+    	<ThemeContext.Provider value={theme}>
+    		<View
+    			toggleTheme={toggleTheme}
+    		/>
+    	</ThemeContext.Provider>
+    );
 }
 
 export default Container;
--- src/components/view.js
+++ src/components/view.js
@@ -1,43 +1,52 @@
 import React from 'react';
-import {ThemeContext} from "./theme-context";
+import {ThemeContext} from './theme-context';
+import {useState, useEffect, useContext} from 'react';
 
 const View = props => {
 
-	const { items, text, addItem, updateText, changeTheme } = props;
+	const [text, setText] = useState("");
+	const [items, setItems] = useState([]);
+	const [count, setCount] = useState(0);
+	const theme = useContext(ThemeContext);
+
+	useEffect(() => {
+		document.title = `You have ${count} tasks!`;
+	}, [count]);
+
+	function addItem(e){
+		e.preventDefault();
+		setItems([...items, text]);
+		setCount(count + 1);
+		setText("");
+	}
 
 	return (
-		<ThemeContext.Consumer>
-			{
-				theme => (
-					<div className={`${theme}--container`}>
-						<div className={`${theme}--wrapper`}>
-							<form className={`${theme}--form-container`} >
-								<input
-									className={`${theme}--input`}
-									placeholder="e.g. Water the plants"
-									value={text}
-									onChange={e=>updateText(e)}
-								/>
-								<input
-									type="submit"
-									className={`${theme}--button`}
-									onClick={e=>addItem(e)} 
-									value="ADD"
-								/>
-							</form>
-							<div className={`${theme}--list-container`} >
-								<ul	className={`${theme}--list-proper`}>
-									{
-										items.map(item => <li className={`${theme}--list-item`} >{item}</li>)
-									}
-								</ul>
-							</div>
-							<label className={`${theme}--theme-anchor`} onClick={()=>changeTheme()}>Change Theme</label>
-						</div>
-					</div>
-				)
-			}
-		</ThemeContext.Consumer>
+		<div className={`${theme}--container`}>
+			<div className={`${theme}--wrapper`}>
+				<form className={`${theme}--form-container`} >
+					<input
+						className={`${theme}--input`}
+						placeholder="e.g. Water the plants"
+						value={text}
+						onChange={e=>setText(e.target.value)}
+					/>
+					<input
+						type="submit"
+						className={`${theme}--button`}
+						onClick={e=>addItem(e)} 
+						value="ADD"
+					/>
+				</form>
+				<div className={`${theme}--list-container`} >
+					<ul	className={`${theme}--list-proper`}>
+						{
+							items.map(item => <li className={`${theme}--list-item`} >{item}</li>)
+						}
+					</ul>
+				</div>
+				<label className={`${theme}--theme-anchor`} onClick={()=>props.toggleTheme()}>Change Theme</label>
+			</div>
+		</div>
 	);
 }
 
